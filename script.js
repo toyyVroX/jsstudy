8444a81ec75417af9ed20f4a04e06b12730c22f7
toyyVroX <koverdaoleg2@gmail.com>
2025-07-27 15:52:44 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,28 +1,74 @@
-let z = +prompt("Перше число");
-
-for (let i = 1 ; i == 1;){
-    if (z){
-        console.log("зараз число дорівнює", z)
-    }
-    let c = prompt("Яку дію ти хочеш виконати по типу + і *");
-    let y = +prompt("Яке друге число?");
-    
-    if (c == "+"){
-        z = z + y
-    }
-    if (c == "-"){
-        z = z - y
-    }
-    if (c == "/"){
-        z = z / y
-    }
-    if (c == "*"){
-        z = z * y
-    }
-    console.log(z)
-    let a = confirm("Далі рахуємо?");
-    if (a == false){
-        i=0
+class DragAndDrop {
+    selectors = {
+        root: '[data-js-dnd]'
     }
 
+    stateClasses = {
+        isDragging: 'is-dragging',
+    }
+
+    initialState = {
+        offsetX: null,
+        offsetY: null,
+        isDragging: false,
+        currentDraggingElement: null,
+    }
+
+    constructor() {
+        this.state = {...this.initialState}
+        this.bindEvents()
+    }
+
+    resetState() {
+        this.state = {...this.initialState}
+    }
+
+    onPointerDown(event) {
+        const { target, x, y } = event
+        const isDraggable = target.matches(this.selectors.root)
+        
+        if (!isDraggable) {
+            return
+        }
+
+        target.classList.add(this.stateClasses.isDragging)
+
+        const { left, top } = target.getBoundingClientRect()
+
+        this.state = {
+            offsetX: x - left,
+            offsetY: y - top,
+            isDragging: true,
+            currentDraggingElement: target,
+        }
+    }
+
+    onPointerMove(event) {
+        if (!this.state.isDragging) {
+            return
+        }
+
+        const x = event.pageX - this.state.offsetX
+        const y = event.pageY - this.state.offsetY
+
+        this.state.currentDraggingElement.style.left = `${x}px`
+        this.state.currentDraggingElement.style.top = `${y}px`
+    }
+
+    onPointerUp() {
+        if (!this.state.isDragging) {
+            return
+        }
+
+        this.state.currentDraggingElement.classList.remove(this.stateClasses.isDragging)
+        this.resetState()
+    }
+
+    bindEvents() {
+        document.addEventListener('pointerdown', (event) => this.onPointerDown(event))
+        document.addEventListener('pointermove', (event) => this.onPointerMove(event))
+        document.addEventListener('pointerup', () => this.onPointerUp())
+    }
 }
+
+new DragAndDrop()
